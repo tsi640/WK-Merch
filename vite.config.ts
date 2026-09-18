69fe1949121b31ebf4373912372082d4264917bd
tsi640 <torbensimon@gmx.net>
2026-09-18 23:18:50 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Relative asset paths work on username.github.io/repository-name/.
-  base: './',
-})
